c9306f4345dd430913c0db836dc98137e229baec
Gosia Brozek <gosikbrozek1997@gmail.com>
2023-06-13 14:41:24 +0200

    Add structure and states

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
-import './App.css';
+import React, { ChangeEvent, FC, useState } from "react"
+import "./App.css"
 
-function App() {
+const App: FC = () => {
+  const [task, setTask] = useState<string>("")
+  const [deadline, setDeadline] = useState<number>(0)
+  const [todo, setTodo] = useState([])
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {}
   return (
     <div className="App">
-   
+      <div className="header">
+        <div className="inputContainer">
+          <input type="text" placeholder="Task..." onChange={handleChange} />
+          <input
+            type="number"
+            placeholder="Deadline (in days)"
+            onChange={handleChange}
+          />
+        </div>
+        <button>Add task</button>
+      </div>
+      <div className="todoList"></div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
